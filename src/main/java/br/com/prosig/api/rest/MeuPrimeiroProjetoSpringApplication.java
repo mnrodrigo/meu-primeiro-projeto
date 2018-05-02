f3432cb06d744359a30cd80c554de867c99a07ee
Manoel Rodrigo do Nascimento <rodrigo@prosig.com.br>
2018-05-02 17:05:17 -0300

    Adicionado a versao inicial do projeto

---
+++ src/main/java/br/com/prosig/api/rest/MeuPrimeiroProjetoSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.prosig.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+	}
+}
